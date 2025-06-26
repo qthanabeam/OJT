9564fa8cd2e2e372d1a5594798180a87b940a334
Hello Kitty <nhinsangngang@gmail.com>
2025-06-26 07:30:41 +0000

    fix ID for view

--- app/employ/webapp/Component.js
+++ app/employ/webapp/Component.js
@@ -1,91 +1,12 @@
-// sap.ui.define(
-//   [
-//     "sap/ui/core/UIComponent",
-//     "sap/ui/Device",
-//     "sap/ui/model/json/JSONModel",
-//     "sap/m/MessageToast",
-//     "sap/ui/core/BusyIndicator",
-//   ],
-//   function (UIComponent, Device, JSONModel, MessageToast, BusyIndicator) {
-//     "use strict";
-
-//     return UIComponent.extend("ojt.employ.Component", {
-//       metadata: {
-//         manifest: "json",
-//         interfaces: ["sap.ui.core.IAsyncContentCreation"],
-//       },
-
-//       init: function () {
-//         // Call the base component's init function
-//         UIComponent.prototype.init.apply(this, arguments);
-
-//         // Initialize the router
-//         this.getRouter().initialize();
-
-//         // Set device model
-//         var oDeviceModel = new JSONModel(Device);
-//         oDeviceModel.setDefaultBindingMode("OneWay");
-//         this.setModel(oDeviceModel, "device");
-
-//         // Set user model
-//         var oUserModel = new JSONModel({
-//           isAdmin: false,
-//           role: "",
-//           userName: "",
-//           email: "",
-//           scopes: [],
-//         });
-//         this.setModel(oUserModel, "user");
-
-//         // Check user role from XSUAA
-//         this._checkUserRole();
-//       },
-
-//       _checkUserRole: function () {
-//         var that = this;
-//         BusyIndicator.show();
-
-//         fetch("/user-info", {
-//           method: "GET",
-//           headers: {
-//             "Content-Type": "application/json",
-//             Accept: "application/json",
-//           },
-//         })
-//           .then(function (response) {
-//             if (!response.ok) {
-//               throw new Error(
-//                 "Network response was not ok: " + response.status
-//               );
-//             }
-//             return response.json();
-//           })
-//           .then(function (data) {
-//             var oUserModel = that.getModel("user");
-//             var bIsAdmin =
-//               data.scopes && data.scopes.includes("ojt_employ.Admin");
-
-//             oUserModel.setProperty("/isAdmin", bIsAdmin);
-//             oUserModel.setProperty("/role", bIsAdmin ? "Admin" : "Viewer");
-//             oUserModel.setProperty("/userName", data.name || "Unknown");
-//             oUserModel.setProperty("/email", data.email || "");
-//             oUserModel.setProperty("/scopes", data.scopes || []);
-
-//             BusyIndicator.hide();
-//           })
-//           .catch(function (error) {
-//             console.error("Error fetching user info:", error);
-//             BusyIndicator.hide();
-//             MessageToast.show("Không thể lấy thông tin người dùng");
-//             that.getModel("user").setProperty("/role", "Viewer");
-//           });
-//       },
-//     });
-//   }
-// );
 sap.ui.define(
-  ["sap/ui/core/UIComponent", "sap/ui/Device", "sap/ui/model/json/JSONModel"],
-  function (UIComponent, Device, JSONModel) {
+  [
+    "sap/ui/core/UIComponent",
+    "sap/ui/Device",
+    "sap/ui/model/json/JSONModel",
+    "sap/m/MessageToast",
+    "sap/ui/core/BusyIndicator",
+  ],
+  function (UIComponent, Device, JSONModel, MessageToast, BusyIndicator) {
     "use strict";
 
     return UIComponent.extend("ojt.employ.Component", {
@@ -108,13 +29,57 @@ sap.ui.define(
 
         // Set user model
         var oUserModel = new JSONModel({
-          isAdmin: true, // Set to true for testing, change to false for Viewer role
+          isAdmin: false,
+          role: "",
+          userName: "",
+          email: "",
+          scopes: [],
         });
         this.setModel(oUserModel, "user");
 
+        // Check user role from XSUAA
         this._checkUserRole();
       },
 
+      // _checkUserRole: function () {
+      //   var that = this;
+      //   BusyIndicator.show();
+
+      //   fetch("/user-info", {
+      //     method: "GET",
+      //     headers: {
+      //       "Content-Type": "application/json",
+      //       Accept: "application/json",
+      //     },
+      //   })
+      //     .then(function (response) {
+      //       if (!response.ok) {
+      //         throw new Error(
+      //           "Network response was not ok: " + response.status
+      //         );
+      //       }
+      //       return response.json();
+      //     })
+      //     .then(function (data) {
+      //       var oUserModel = that.getModel("user");
+      //       var bIsAdmin =
+      //         data.scopes && data.scopes.includes("ojt_employ.Admin");
+
+      //       oUserModel.setProperty("/isAdmin", bIsAdmin);
+      //       oUserModel.setProperty("/role", bIsAdmin ? "Admin" : "Viewer");
+      //       oUserModel.setProperty("/userName", data.name || "Unknown");
+      //       oUserModel.setProperty("/email", data.email || "");
+      //       oUserModel.setProperty("/scopes", data.scopes || []);
+
+      //       BusyIndicator.hide();
+      //     })
+      //     .catch(function (error) {
+      //       console.error("Error fetching user info:", error);
+      //       BusyIndicator.hide();
+      //       MessageToast.show("Không thể lấy thông tin người dùng");
+      //       that.getModel("user").setProperty("/role", "Viewer");
+      //     });
+      // },
       _checkUserRole: function () {
         var oUserModel = this.getModel("user");
         var bIsAdmin = true;
